724ed47a1577c2963c83c6fbbe3a2bbef03ef7be
tomas <115982948+tomaslmz@users.noreply.github.com>
2023-05-17 21:54:35 -0300

    adição da biblioteca frame

---
+++ src/TelaAbrirConta.java
@@ -0,0 +1,49 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
+
+public class TelaAbrirConta extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaAbrirConta frame = new TelaAbrirConta();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaAbrirConta() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTextPane txtNome = new JTextPane();
+		txtNome.setBounds(103, 36, 244, 27);
+		contentPane.add(txtNome);
+		
+		JLabel lblNewLabel = new JLabel("Nome");
+		lblNewLabel.setBounds(70, 36, 46, 14);
+		contentPane.add(lblNewLabel);
+	}
+}
